c40f7031e5d55539c2755c51476d4f984ddb44a7
agconti <agc11d@gmail.com>
2017-11-04 19:45:41 -0400

    feat(Grid/Grid): basic row grid

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,12 +1,19 @@
 import './Grid.css'
+import data from '../data'
+
 const sources = [
     'https://media.giphy.com/media/lAK7ACXBaEy4/giphy.gif',
     'https://media.giphy.com/media/26FmPzA1QuRAIrkkM/giphy.gif',
     'https://media.giphy.com/media/xT9IgmVz0jdY4lt4Dm/giphy.gif',
 ]
-const Image = src => `<img class="grid__image" src=${src} /> `
+
+const Image = ({ name, thumbnailUrl }) => `
+    <div class="grid__item">
+        <img class="image" src="${thumbnailUrl}" alt="${name}" />
+    </div>
+`
 export const Grid = () => `
     <section class="grid">
-    ${sources.map(source => Image(source)).join('')}
+        ${data.value.map(image => Image(image)).join('')}
     </section>
 `
