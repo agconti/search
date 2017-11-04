4149e72f4de52f12dada4ce050d1553b0f4cf1ba
agconti <agc11d@gmail.com>
2017-11-04 12:51:53 -0400

    chore: remove extra boilerplate

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css'
 
 export const render = () => {
     return `
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div class="App">
+        <header class="App-header">
+          <h1 class="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p class="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
     </div>`
