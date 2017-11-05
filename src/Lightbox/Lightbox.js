580c87bb61c4898fce02bdb8e30db567490f9347
agconti <agc11d@gmail.com>
2017-11-05 17:56:54 -0500

    feat(Lightbox/Lightbox): support keyboard events

--- src/Lightbox/Lightbox.js
+++ src/Lightbox/Lightbox.js
@@ -2,6 +2,9 @@ import './Lightbox.css'
 import { toggleActiveItem } from '../reducers'
 import store from '../store'
 
+const navigate = id => () => id && store.dispatch(toggleActiveItem(id))
+const exitLightBox = () => store.dispatch(toggleActiveItem(null))
+
 const Div = className => {
     const el = document.createElement('div')
     el.className = className
@@ -18,21 +21,38 @@ const Image = ({ name, contentUrl }) => {
 const Close = () => {
     const close = Div('close')
     close.innerHTML = '<i class="fa fa-times-circle fa-2x" aria-hidden="true"></i>'
-    close.onclick = () => store.dispatch(toggleActiveItem(null))
+    close.onclick = exitLightBox
     return close
 }
 const Arrow = (direction, id) => {
     const arrow = Div(`arrow arrow--${direction}`)
     arrow.innerHTML = `<i class="fa fa-angle-${direction} fa-2x" aria-hidden="true"></i>`
-    arrow.onclick = () => store.dispatch(toggleActiveItem(id))
+    arrow.onclick = navigate(id)
     return arrow
 }
 
 const Overlay = () => {
     const overlay = Div('overlay overlay--active')
-    overlay.onclick = () => store.dispatch(toggleActiveItem(null))
+    overlay.onclick = exitLightBox
     return overlay
 }
+
+const navigateOnKeyPress = (previousItemId, nextItemId) => ({ key }) => {
+    const handler = {
+        ArrowLeft: navigate(previousItemId),
+        ArrowRight: navigate(nextItemId),
+        Escape: exitLightBox,
+    }[key]
+
+    handler()
+}
+
+const supportKeyboardNavigation = (el, previousItem, nextItem) => {
+    const handler = navigateOnKeyPress(previousItem, nextItem)
+    document.addEventListener('keyup', handler, true)
+    el.addEventListener('DOMNodeRemoved', () => document.removeEventListener('keyup', handler, true))
+}
+
 export const Lightbox = (el, activeImage, previousItem, nextItem) => {
     el.innerHTML = ''
     if (!activeImage) {
@@ -40,6 +60,7 @@ export const Lightbox = (el, activeImage, previousItem, nextItem) => {
     }
     const lightbox = Div('lightbox')
     const container = Div('container')
+    supportKeyboardNavigation(el, previousItem, nextItem)
 
     previousItem && container.appendChild(Arrow('left', previousItem))
     container.appendChild(Image(activeImage))
