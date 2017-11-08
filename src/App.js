ba40fad57c830b635908b7da22683700ea275f03
Conti <agc11d@gmail.com>
2017-11-08 12:32:14 -0500

    Revert "Revert "Revert "feat(src/SearchBar): add search bar"""
    This reverts commit 1583a91a34624cb0ad8733f3bb8024c18e2f927e.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css'
-import { SearchBar } from './SearchBar'
 import store from './store'
 
 const App = () => `
@@ -8,7 +7,6 @@ const App = () => `
         <div id="lightbox"></div>
         <header class="header">
           <h1 class="title">Simple Search App</h1>
-          <div class="searchbar"></div>
         </header>
         <section class="grid"></section>
     </section>
@@ -17,11 +15,9 @@ const App = () => `
 export default App
 
 store.subscribe(() => {
-    const rootElement = document.getElementById('root')
-
     if (store.getState().activeItem) {
-        rootElement.style.position = 'fixed'
+        document.getElementById('root').style.position = 'fixed'
         return
     }
-    rootElement.style.position = 'initial'
+    document.getElementById('root').style.position = 'initial'
 })
